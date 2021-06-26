36367e4fb65f83ec345e5a7f15d9dd3a35a25431
cedi <cedisto@gmx.ch>
2021-06-26 14:51:47 +0200

    Update backgroundless-pins.js

--- backgroundless-pins.js
+++ backgroundless-pins.js
@@ -18,13 +18,13 @@ Hooks.on("init", () => {
 	
 	// Keep tooltip always visible
 	// Though could make an option out of that too. Would be nicer
-	Note.prototype.refresh = function() {
-	    this.position.set(this.data.x, this.data.y);
-	    this.controlIcon.border.visible = this._hover;
-	    this.tooltip.visible = true;
-	    this.visible = this.entry?.testUserPermission(game.user, "LIMITED") ?? true;
-	    return this;
-	}
+    Note.prototype.refresh = function() {
+	this.position.set(this.data.x, this.data.y);
+	this.controlIcon.border.visible = this._hover;
+	this.tooltip.visible = true;
+	this.visible = this.entry?.testUserPermission(game.user, "LIMITED") ?? true;
+	return this;
+    }
 	
 	// Override map notes to use the BackgroundlessControlIcon 
 	//Note.prototype._drawTooltip = function() {
